c4c6968106d8d394e9d935ddd4821e2cf11b4ef1
nrnsaheb <nooruddinsaheb@gmail.com>
2022-07-27 14:54:06 +0530

    1 file modified, 1 added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,7 +3,7 @@ import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
+  templateUrl: 'products.component.html',
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
